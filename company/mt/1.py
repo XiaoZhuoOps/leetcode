c65f0824dfebaa838e90e0ac83467c0cecd6784f
XiaoZhuoOps <67407540+XiaoZhuoOps@users.noreply.github.com>
2024-03-10 01:03:51 +0800

    xx

--- company/mt/1.py
+++ company/mt/1.py
@@ -1,132 +1,121 @@
 """
-1 
-2
-3
+1
+2 x y 
+3 
 4
 """
 # class Solution:
-#     def f(n, t, nums):
-#         nums.sort(reverse=True)
-#         ct = 0
-#         ans = 0
-#         while nums:
-#             if nums[-1] < ct + t:
-#                 ans += 1
+#     def f(x, y):
+#         a = max(x, y) - min(x, y)
+#         if a >= min(x, y):
+#             return min(x, y)
+#         else:
+#             return 2*(min(x,y)-a)//3 + a
+# n = int(input())
+# arr = [0 for _ in range(n)]
+# for i in range(n):
+#     arr[i] = list(map(int, input().split()))
+# for i in range(n):
+#     print(Solution.f(arr[i][0], arr[i][1]))
+
+"""
+2 
+4 [1]
+"""
+# class Solution:
+#     def f(n, nums):
+#         ls, rs = [0 for _ in range(n+1)], [0 for _ in range(n+1)]
+#         ans = float('inf')
+#         for k in range(1,n+1):
+#             if nums[k-1] >= 0:
+#                 ls[k] = ls[k-1]+1
 #             else:
-#                 ct += t
-#             nums.pop(-1)
+#                 ls[k] = ls[k-1]
+#         for k in range(n-1, -1, -1):
+#             if nums[k] <= 0:
+#                 rs[k] = rs[k+1]+1
+#             else:
+#                 rs[k] = rs[k+1]
+#         for k in range(n+1):
+#             ans = min(ans, ls[k]+rs[k])
 #         return ans
 
-
-# line = list(map(int, input().split()))
-# n, t = line[0], line[1]
-# nums = list(map(int, input().split()))
-# print(Solution.f(n, t, nums))
-"""
-1 
-2
-3
-4
-"""
-# class Solution:
-#     def f(n, m, k, move):
-#         vis = [[False for _ in range(m)] for _ in range(n)]
-#         vis[0][0] = True
-#         cnt = n*m-1
-#         x, y = 0, 0
-#         for i in range(k):
-#             if move[i] == 'W':
-#                 x, y = x-1, y
-#             if move[i] == 'A':
-#                 x, y = x, y-1
-#             if move[i] == 'S':
-#                 x, y = x+1, y
-#             if move[i] == 'D':
-#                 x, y = x, y+1
-#             if not vis[x][y]:
-#                 cnt -= 1
-#             vis[x][y] = True
-#             if cnt == 0:
-#                 print("Yes")
-#                 print(i+1)
-#                 return
-#         print("No")
-#         print(cnt)
-
-
-# line = list(map(int, input().split()))
-# n, m, k = line[0], line[1], line[2]
-# move = str(input())
-# Solution.f(n, m, k, move)
-"""
-1 moni
-2 
-3
-4
-"""
-# import collections
-# class Solution:
-#     def f(n, ps):
-#         # s = [ps[-1]]
-#         dq = collections.deque()
-#         dq.append(ps[-1])
-#         for i in range(n-2, -1, -1):
-#             # s = [ps[i]] + s
-#             # s.insert(0, ps[i])
-#             dq.appendleft(ps[i])
-#             # s = s[-2:] + s[:-2]
-#             dq.appendleft(dq.pop())
-#             dq.appendleft(dq.pop())
-#         print(" ".join(list(map(str, dq))))
-
-
-# n = int(input())
-# ps = list(map(int, input().split()))
-# Solution.f(n, ps)
-"""
-1 a[i]+a[k]=3a[j]
-2 
-3
-4
-"""
-# import bisect
-# class Solution:
-#     def f(nums):
-#         n = len(nums)
-#         ans = 0
-#         for j in range(1, n-1):
-#             tmp = sorted(nums[j+1:])
-#             for i in range(j):
-#                 t = 3*nums[j]-nums[i]
-#                 idx = bisect.bisect_left(tmp, t)
-#                 if 0 <= idx < len(tmp) and tmp[idx] == t:
-#                     ans += (bisect.bisect_right(tmp, t)-idx)
-#         print(ans)
 # n = int(input())
 # nums = list(map(int, input().split()))
-# Solution.f(nums)
+# print(Solution.f(n, nums))
+
 """
+1 dp
 1 
-2 
+5
+1 5 7 5 5 
+10 5 5 9 10
+2 count[i][0] 
 3
 4
-"""
+# """
+# import heapq
+# class Solution:
+#     def f(n, nums1, nums2):
+#         m1, m2 = {}, {}
+#         for i in range(n):
+#             if nums1[i] not in m1:
+#                 m1[nums1[i]] = 0
+#             m1[nums1[i]] += 1
+
+#         for i in range(n):
+#             if nums2[i] not in m2:
+#                 m2[nums2[i]] = 0
+#             m2[nums2[i]] += 1
+        
+#         half = n//2 if n%2 == 0 else n//2+1
+#         h = []
+#         for k, v in m1.items():
+#             heapq.heappush(h, (v, k))
+#         ans = float('inf')
+#         while h:
+#             v, k = heapq.heappop(h)
+#             if v >= half:
+#                 return 0
+#             else:
+#                 if v+m2.get(k,0) >= half:
+#                     ans = min(ans, half-v)
+#         return -1 if ans == float('inf') else ans
+
+# n = int(input())
+# nums1 = list(map(int, input().split()))
+# nums2 = list(map(int, input().split()))
+# print(Solution.f(n, nums1, nums2))
+
+# class Solution:
+#     def f(n, nums):
+#         map = {}
+#         for i in range(1,n+1):
+#             if nums[i-1] not in map:
+#                 map[nums[i-1]] = []
+#             map[nums[i-1]].append(i)
+#         a, b = [], []
+#         for k, v in map.items():
+#             m = len(v)
+#             p = m//2 if m%2 == 0 else m//2+1
+#             a += v[:p]
+#             b += v[p:]
+#         a.sort()
+#         b.sort()
+#         astr, bstr = [], []
+#         for e in a:
+#             astr.append(str(e))
+#         for e in b:
+#             bstr.append(str(e))
+
+#         print(" ".join(astr))
+#         print(" ".join(bstr))
 
 
-class Solution:
-    def f(n, ms):
-        def dfs(idx):
-            nonlocal n, ms
-            res = ms[idx-1]
-            lch, rch = 0, 0
-            if 2*idx <= n:
-                lch = dfs(2*idx)
-            if 2*idx+1 <= n:
-                rch = dfs(2*idx+1)
-            return res+max(lch, rch)
-        print(dfs(1))
+# line1 = list(map(int, input().split()))
+# n = line1[0]
+# nums = list(map(int, input().split()))
+# Solution.f(n, nums)
 
-
-n = int(input())
-ms = list(map(int, input().split()))
-Solution.f(n, ms)
+s = str(input())
+print(s+s.reverse()+"wowwow"+s+s.reverse()+"wow")
